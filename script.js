f1e820c4d8a99ad1b179ca04bee5f1f3abd5c6e7
amiko-srg <amikooo333@gmail.com>
2025-05-06 12:39:36 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var omina ='Omina'
+console.log(omina);
+
+
+var savol = prompt('Ismingizni kiriting!')
+var savol = prompt('Yoshingizni kiriting!')
+var savol = prompt('Manzilingizni kiriting!')
+var javob = alert('Mening ismim Omina, man 16 yoshdaman, Toshkentda tugulganman')
